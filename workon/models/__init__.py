a058181059a0256b8788bc5436d228feb36489ff
Dalou <autrusseau.damien@gmail.com>
2016-02-29 08:47:26 +0100

    fix

---
+++ workon/models/__init__.py
@@ -0,0 +1,10 @@
+from ..modules.user.models import User
+from ..modules.tree.models import Tree
+from ..modules.unique.models import Unique
+from ..modules.google.models import GoogleAPISettings
+from ..modules.emailing.models import *
+from ..modules.selection.models import Selection
+from ..modules.stripe.models import *
+from ..modules.social.models import *
+
+from .aggregate_if import CountIf, SumIf, AvgIf, MaxIf, MinIf
