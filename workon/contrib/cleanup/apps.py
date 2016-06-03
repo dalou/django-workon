5053635f0316b913758f02fc927772d5c6c6f5bb
Dalou <autrusseau.damien@gmail.com>
2016-06-03 07:39:57 +0200

    fix

---
+++ workon/contrib/cleanup/apps.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+'''
+    AppConfig for django-cleanup, prepare the cache and connect signal handlers
+'''
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+from . import cache, handlers
+
+
+class CleanupConfig(AppConfig):
+    name = 'workon.contrib.cleanup'
+    label = 'workon.cleanup'
+    verbose_name = _("File and Image Cleanup")
+
+    def ready(self):
+        cache.prepare()
+        handlers.connect()
