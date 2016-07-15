7abff999a44f2276fd8f1ceb3e5960b38e965d9c
Dalou <autrusseau.damien@gmail.com>
2016-07-15 08:22:52 +0200

    fix

--- workon/admin.py
+++ workon/admin.py
@@ -6,3 +6,24 @@ from .contrib.stripe.admin import *
 from .contrib.auth.admin import *
 from .contrib.admin.admin import *
 # # from .setting import SettingsAdmin
+
+
+from django.contrib import admin
+
+class ViewableOnly(admin.ModelAdmin):
+
+
+    def get_readonly_fields(self, request, obj=None):
+        return self.fields or [f.name for f in self.model._meta.fields]
+
+    def has_add_permission(self, request):
+        return False
+
+    # Allow viewing objects but not actually changing them
+    def has_change_permission(self, request, obj=None):
+        if request.method not in ('GET', 'HEAD'):
+            return False
+        return super(CDRCommunicationAdmin, self).has_change_permission(request, obj)
+
+    def has_delete_permission(self, request, obj=None):
+        return False
