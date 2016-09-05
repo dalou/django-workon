3645b7e9ec1c9ab47b96e1ccb33987f24e2b5b2c
Dalou <autrusseau.damien@gmail.com>
2016-09-05 12:16:20 +0200

    fix

--- workon/utils/text/__init__.py
+++ workon/utils/text/__init__.py
@@ -2,23 +2,38 @@
 
 import unicodedata
 from django.utils.encoding import force_str, force_text
+from django.utils.text import slugify as django_slugify
 
-def forceunicode(str):
+def forceunicode(string):
     enc = 'utf-8'
-    try: return str.decode(enc)
+    try: return string.decode(enc)
     except:
         enc = 'ISO-8859-1'
-        try: return str.decode(enc)
+        try: return string.decode(enc)
         except:
             enc = 'ascii'
-            try: return str.decode(enc)
-            except: return str
-    return str
+            try: return string.decode(enc)
+            except: return string
+    return string
 
-def normalize(str):
-    if not str: return ""
-    if not type(str) == type(unicode): str = forceunicode(str)
-    str = str.replace(u'’', "'")
-    str = str.replace(u'–', "-")
-    return unicodedata.normalize('NFKD',str).encode('ascii','ignore').lower().strip()
+def normalize(string):
+    if not string: return ""
+    if not type(string) == type(unicode): string = forceunicode(string)
+    string = string.replace(u'“', '"')
+    string = string.replace(u'”', '"')
+    string = string.replace(u'’', "'")
+    string = string.replace(u'–', "-")
+    return unicodedata.normalize('NFKD',string).encode('ascii','ignore').lower().strip()
 
+
+def normalize_hard(string):
+    if not string: return ""
+    if not type(string) == type(unicode): string = forceunicode(string)
+    string = normalize(string)
+    string = string.replace(u'"', '')
+    string = string.replace(u"'", '')
+    string = string.replace(u'-', '')
+    return unicodedata.normalize('NFKD',string).encode('ascii','ignore').lower().strip()
+
+def slugify(string, allow_unicode=False):
+    return django_slugify(string)
