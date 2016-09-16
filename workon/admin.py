1638ab7d7bb96e5d66ac0dfc9ad2f4a2d59eb243
Dalou <autrusseau.damien@gmail.com>
2016-09-16 11:29:07 +0200

    fix

--- workon/admin.py
+++ workon/admin.py
@@ -1,8 +1,8 @@
 from .contrib.user.admin import UserAdmin
 from .contrib.tree.admin import TreeAdmin
-from .contrib.google.admin import GoogleAPISettingsAdmin
+# from .contrib.google.admin import GoogleAPISettingsAdmin
 from .contrib.unique.admin import UniqueAdmin
-from .contrib.stripe.admin import *
+# from .contrib.stripe.admin import *
 from .contrib.auth.admin import *
 from .contrib.admin.admin import *
 # # from .setting import SettingsAdmin
