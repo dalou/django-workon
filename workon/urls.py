30e8bbe3b9b799e0a9ee2f6850bbff17d8c424dd
Dalou <autrusseau.damien@gmail.com>
2016-09-19 19:16:22 +0200

    fix

--- workon/urls.py
+++ workon/urls.py
@@ -21,29 +21,6 @@ if 'workon.contrib.stripe' in settings.INSTALLED_APPS:
 urlpatterns = [
     url(r'^', include(includes, namespace="workon"))
 ]
-
-include_urls = []
-
-if 'workon.contrib.auth' in settings.INSTALLED_APPS:
-    include_urls += [
-        url(r'^auth/', include("workon.contrib.auth.urls")),
-    ]
-
-if 'workon.contrib.stripe' in settings.INSTALLED_APPS:
-    include_urls += [
-        url(r'^stripe/', include("workon.contrib.stripe.urls")),
-    ]
-
-if 'workon.contrib.flow' in settings.INSTALLED_APPS:
-    include_urls += [
-        url(r'^flow/', include("workon.contrib.flow.urls")),
-    ]
-
-if 'workon.contrib.select2' in settings.INSTALLED_APPS:
-    include_urls += [
-        url(r'^select2/', include("workon.contrib.select2.urls")),
-    ]
-
 # urlpatterns.append(
 #     url(r'^', include(include_urls, namespace="workon"))
 # )
