675bdd2f80038a3542a73b77b10d5a938765afd9
Dalou <autrusseau.damien@gmail.com>
2016-09-22 02:00:47 +0200

    fix

--- workon/utils/__init__.py
+++ workon/utils/__init__.py
@@ -37,25 +37,6 @@ def get_project_root(default=""):
             return getattr(settings, attr)
     return default
 
-def jsonify(obj):
-    if obj is None:
-        return "{}"
-    if isinstance(obj,dict):
-        return json.dumps(obj)
-    elif isinstance(obj,list):
-        return json.dumps(obj)
-    else:
-        # obj = re.sub(r'([\w\d_]+)\:', '"\\1":', obj)
-        obj = re.sub(r'\'', '"', obj)
-        obj = re.sub(r'\/\/\s*[\w\s\d]+', '', obj)
-        obj = re.sub(r'Date\.UTC\(.+\)', '""', obj)
-
-        try:
-            return json.dumps(json.loads(obj))
-        except:
-            return json.loads(json.dumps(obj))
-
-
 def get_ip_address(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
