01a3e8ccd59dcd68e8f16d8643a3c3a9cbf84f6e
Dalou <autrusseau.damien@gmail.com>
2016-09-29 22:29:58 +0200

    fix

--- workon/contrib/chart/templatetags.py
+++ workon/contrib/chart/templatetags.py
@@ -9,11 +9,17 @@ WORKON_CHARTS = {}
 
 def highchart_render(context, *args, **kwargs):
     if args:
-        chart = WORKON_CHARTS.get(args[0])
-        if chart:
+        if isinstance(args[0], dict):
             return {
-                'chart': chart.get('method')(context, *args, **kwargs),
-                'label': kwargs.get('label', chart.get('label',  ''))
+                'chart': args[0],
+                'label': kwargs.get('label', args[0].get('label',  ''))
             }
+        else:
+            chart = WORKON_CHARTS.get(args[0])
+            if chart:
+                return {
+                    'chart': chart.get('method')(context, *args, **kwargs),
+                    'label': kwargs.get('label', chart.get('label',  ''))
+                }
 
 register.inclusion_tag('workon/chart/highchart_chart.html', takes_context=True, name='highchart_render')(highchart_render)
